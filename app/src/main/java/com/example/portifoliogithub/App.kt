bde378ba146cf5f3e337ebc002a70064340a3c60
CardosofGui <cardosof.gui@gmail.com>
2021-07-27 01:45:15 -0300

    Implementing Data Module

--- app/src/main/java/com/example/portifoliogithub/App.kt
+++ app/src/main/java/com/example/portifoliogithub/App.kt
@@ -1,6 +1,7 @@
 package com.example.portifoliogithub
 
 import android.app.Application
+import com.example.portifoliogithub.data.di.DataModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -12,5 +13,7 @@ class App : Application() {
         startKoin{
             androidContext(this@App)
         }
+
+        DataModule.load()
     }
 }
---
+++ app/src/main/java/com/example/portifoliogithub/domain/Repo.kt
@@ -0,0 +1,4 @@
+package com.example.portifoliogithub.domain
+
+class Repo {
+}
